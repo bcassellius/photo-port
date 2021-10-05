d5fcd8099b84aed865674d56682d68ae0f3ff61f
Bri <bhilliker@gmail.com>
2021-10-05 13:35:20 -0500

    created 4 passing tests in Nav component

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -4,20 +4,23 @@ import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
 afterEach(cleanup);
+
 describe('Nav component', () => {
     // baseline test
     it('renders', () => {
         render(<Nav />);
-    });
-
+    })
+   
     // snapshot test
     it('matches snapshot', () => {
         const { asFragment } = render(<Nav />);
+        //assert calue comparison
         expect(asFragment()).toMatchSnapshot();
     });
+
 })
 
-//emoji test
+// emoji test
 describe('emoji is visible', () => {
     it('inserts emoji into the h2', () => {
         const { getByLabelText } = render(<Nav />);
@@ -25,11 +28,13 @@ describe('emoji is visible', () => {
     });
 })
 
-// links test
+//link visibility test
 describe('links are visible', () => {
-    it('inserts text into the links', () => {
+    it('inserts text into the lins', () => {
+        //arrange
         const { getByTestId } = render(<Nav />);
+        //assert
         expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-        expect(getByTestId('about')).toHaveTextContent('About me');
+        expect(getByTestId('about')).toHaveTextContent('About Me');
     });
 })
