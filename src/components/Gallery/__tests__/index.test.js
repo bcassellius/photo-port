9f60e7addbdff3260c269e5b2f5681ad6ce93ae3
Bri <bhilliker@gmail.com>
2021-10-11 10:08:49 -0500

    added tests

--- src/components/Gallery/__tests__/index.test.js
+++ src/components/Gallery/__tests__/index.test.js
@@ -3,7 +3,7 @@ import { render, cleanup } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
 import Gallery from '..';
 
-const portrait = { item: "portraits", description: "Portraits of people in my life"};
+const portrait = { name: "portraits", description: "Portraits of people in my life"};
 
 afterEach(cleanup)
 
--- src/components/Modal/__tests__/test.index.js
+++ src/components/Modal/__tests__/test.index.js
@@ -5,39 +5,44 @@ import Modal from '..';
 
 afterEach(cleanup);
 
-
 const currentPhoto = {
     name: 'Park bench',
     category: 'landscape',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
     index: 1
-}
+};
 
 const mockToggleModal = jest.fn();
 
 describe('Modal component', () => {
-    //baseline test
     it('renders', () => {
-        render (<Modal />);
-    });
+        render(<Modal
+            onClose={mockToggleModal}
+            currentPhoto={currentPhoto}
+        />);
+    })
     
     //snapshot test
-    it('matches snapshot', () => {
-        const { asFragment } = render(<Modal />)
-        expect(asFragment()).toMatchSnapshot() 
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<Modal
+          onClose={mockToggleModal}
+          currentPhoto={currentPhoto}
+        />);
+    
+        expect(asFragment()).toMatchSnapshot();
     });
 })
 
 describe('Click Event', () => {
     it('calls onClose handler', () => {
-        const { getByText } = render (<Modal
+        const { getByText } = render(<Modal
         //Arrange: render modal
         onClose={mockToggleModal}
         currentPhoto={currentPhoto}
         />);
         //Act: Simulate click event
-        fireEvent.click(getByText('Close this image'));
+        fireEvent.click(getByText('Close this modal'))
         //Assert: Expected matcher
         expect(mockToggleModal).toHaveBeenCalledTimes(1);
-    })
+    });
 })
